6cf7f2f0c8f239042f592eb7261c1873f5eff123
Dale Wright <dale.wright@mbhci.nhs.uk>
2023-06-08 16:19:23 +0100

    V0.1 working

--- UHMB_Commentary_Table.js
+++ UHMB_Commentary_Table.js
@@ -1,23 +1,23 @@
 /*globals define*/
-define( ["qlik", "jquery", "text!./style.css"], function ( qlik, $, cssContent ) {
+define(["qlik", "jquery", "text!./style.css"], function (qlik, $, cssContent) {
 	'use strict';
-	$( "<style>" ).html( cssContent ).appendTo( "head" );
-	function createRows ( rows, dimensionInfo ) {
+	$("<style>").html(cssContent).appendTo("head");
+	function createRows(rows, dimensionInfo) {
 		var html = "";
-		rows.forEach( function ( row ) {
+		rows.forEach(function (row) {
 			html += '<tr>';
-			row.forEach( function ( cell, key ) {
-				if ( cell.qIsOtherCell ) {
+			row.forEach(function (cell, key) {
+				if (cell.qIsOtherCell) {
 					cell.qText = dimensionInfo[key].othersLabel;
 				}
 				html += "<td ";
-				if ( !isNaN( cell.qNum ) ) {
+				if (!isNaN(cell.qNum)) {
 					html += "class='numeric'";
 				}
 				html += '>' + cell.qText + '</td>';
-			} );
+			});
 			html += '</tr>';
-		} );
+		});
 		return html;
 	}
 
@@ -51,54 +51,157 @@ define( ["qlik", "jquery", "text!./style.css"], function ( qlik, $, cssContent )
 				},
 				settings: {
 					uses: "settings"
-				}
+				},
+				customSection: {
+					component: "expandable-items",
+					label: "Formatting",
+					items: {
+						Header1: {
+							type: "items",
+							label: "Settings",
+							items: {
+								
+								DefaultFont: {
+									type: "boolean",
+									label: "Use Default Font/Size",
+									ref: "DefaultFontFlag",
+									defaultValue: true
+								},
+								CustomFontSizeHeader: {
+									ref: "CustFontSizeHeader",
+									type: "string",
+									label: "Header Custom Font Size",
+									expression: "optional",
+									defaultValue: "14px",
+									show: function (data) {
+										return !data.DefaultFontFlag;
+									}
+
+								},
+								CustomFontFamilyHeader: {
+									ref: "CustomFontFamilyHeader",
+									type: "string",
+									label: "Header Custom Font Family",
+									expression: "optional",
+									defaultValue: "Arial, Helvetica, sans-serif",
+									show: function (data) {
+										return !data.DefaultFontFlag;
+									}
+
+								},
+								CustomFontColHeader: {
+									label: "Header Custom Colour",
+									component: "color-picker",
+									ref: "CustomFontColHeader",
+									type: "object",
+	
+									defaultValue: {
+										color: "#ffffff",
+										index: "-1"
+									},
+									show: function (data) {
+										return !data.DefaultFontFlag;
+									}
+								},
+								CustomFontSizeBody: {
+									ref: "CustFontSizeBody",
+									type: "string",
+									label: "Body Custom Font Size (CSS)",
+									expression: "optional",
+									defaultValue: "14px",
+									show: function (data) {
+										return !data.DefaultFontFlag;
+									}
+
+								},
+								CustomFontFamilyBody: {
+									ref: "CustomFontFamilyBody",
+									type: "string",
+									label: "Body Custom Font Family (CSS)",
+									expression: "optional",
+									defaultValue: "Arial, Helvetica, sans-serif",
+									show: function (data) {
+										return !data.DefaultFontFlag;
+									}
+
+								},
+								CustomFontColBody: {
+									label: "Body Custom Colour",
+									component: "color-picker",
+									ref: "CustomFontColBody",
+									type: "object",
+	
+									defaultValue: {
+										color: "#ffffff",
+										index: "-1"
+									},
+									show: function (data) {
+										return !data.DefaultFontFlag;
+									}
+								}
+
+							}
+						
+
+						}
+					}
+				},
 			}
 		},
 		snapshot: {
 			canTakeSnapshot: true
 		},
-		paint: function ( $element, layout ) {
-			var html = "<table><thead><tr>", self = this,
+		paint: function ($element, layout) {
+			var defFont = layout.DefaultFontFlag;
+			var HeaderStyle= '';
+			var BodyStyle = '';
+			if(defFont == false){
+				HeaderStyle = `style = "font-size:${layout.CustFontSizeHeader}; font-family:${layout.CustomFontFamilyHeader};color: ${layout.CustomFontColHeader.color};"`;
+				BodyStyle = `style = "font-size:${layout.CustFontSizeBody}; font-family:${layout.CustomFontFamilyBody};color: ${layout.CustomFontColBody.color};"`;
+			}
+
+			var html = `<table ><thead ${HeaderStyle}><tr>`, self = this,
 				morebutton = false,
 				hypercube = layout.qHyperCube,
 				rowcount = hypercube.qDataPages[0].qMatrix.length,
 				colcount = hypercube.qDimensionInfo.length + hypercube.qMeasureInfo.length;
 			//render titles
-			hypercube.qDimensionInfo.forEach( function ( cell ) {
+
+			hypercube.qDimensionInfo.forEach(function (cell) {
 				html += '<th>' + cell.qFallbackTitle + '</th>';
-			} );
-			hypercube.qMeasureInfo.forEach( function ( cell ) {
+			});
+			hypercube.qMeasureInfo.forEach(function (cell) {
 				html += '<th>' + cell.qFallbackTitle + '</th>';
-			} );
-			html += "</tr></thead><tbody>";
+			});
+			html += `</tr></thead><tbody ${BodyStyle}>`;
 			//render data
-			html += createRows( hypercube.qDataPages[0].qMatrix, hypercube.qDimensionInfo );
+			html += createRows(hypercube.qDataPages[0].qMatrix, hypercube.qDimensionInfo);
 			html += "</tbody></table>";
 			//add 'more...' button
-			if ( hypercube.qSize.qcy > rowcount ) {
+			if (hypercube.qSize.qcy > rowcount) {
 				html += "<button class='more'>More...</button>";
 				morebutton = true;
 			}
-			$element.html( html );
-			if ( morebutton ) {
-				$element.find( ".more" ).on( "click", function () {
+			$element.html(html);
+			if (morebutton) {
+				$element.find(".more").on("click", function () {
 					var requestPage = [{
 						qTop: rowcount,
 						qLeft: 0,
 						qWidth: colcount,
-						qHeight: Math.min( 50, hypercube.qSize.qcy - rowcount )
+						qHeight: Math.min(50, hypercube.qSize.qcy - rowcount)
 					}];
-					self.backendApi.getData( requestPage ).then( function ( dataPages ) {
+					self.backendApi.getData(requestPage).then(function (dataPages) {
 						rowcount += dataPages[0].qMatrix.length;
-						if ( rowcount >= hypercube.qSize.qcy ) {
-							$element.find( ".more" ).hide();
+						if (rowcount >= hypercube.qSize.qcy) {
+							$element.find(".more").hide();
 						}
-						var html = createRows( dataPages[0].qMatrix, hypercube.qDimensionInfo );
-						$element.find( "tbody" ).append( html );
-					} );
-				} );
+						var html = createRows(dataPages[0].qMatrix, hypercube.qDimensionInfo);
+						$element.find("tbody").append(html);
+					});
+				});
 			}
 			return qlik.Promise.resolve();
 		}
 	};
-} );
+});
